6d8c3469a77dd2dd2fc16a2807d0e1cdf5156e7f
vaarunx <varunrishwandh2001@gmail.com>
2025-03-31 19:55:38 -0400

    Completed Lab of Assignment 5

---
+++ index.js
@@ -0,0 +1,13 @@
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+Lab5(app);
+Hello(app);
+
+app.listen(process.env.PORT || 4000);
